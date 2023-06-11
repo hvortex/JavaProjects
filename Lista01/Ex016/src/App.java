77c75e659a45ec8dce6a50a335a4d75bf64aed04
Hebert Magalhães Soares <hebertms2009@hotmail.com>
2023-06-11 13:52:40 -0300

    Final

--- Lista01/Ex016/src/App.java
+++ Lista01/Ex016/src/App.java
@@ -7,27 +7,15 @@ public class App {
     System.out.print("Digite o tamanho da área a ser pintada em metros quadrados");
         double areaPintada = teclado.nextDouble();
 
-        double coberturaTinta = 6.0; 
-        double quantidadeTinta = areaPintada / coberturaTinta;
-        quantidadeTinta = quantidadeTinta * 1.1;
-        int latas18Litros = (int) Math.ceil(quantidadeTinta / 18.0);
-        int galoes36Litros = (int) Math.ceil(quantidadeTinta / 3.6); 
+        double coberturaTinta = 3.0; 
+        double quantidadeDeTinta = areaPintada / coberturaTinta;
+
+        int latas18Litros = (int) Math.ceil(quantidadeDeTinta / 18.0);
 
         double precoLata18Litros = latas18Litros * 80.0;
-        double precoGalao36Litros = galoes36Litros * 25.0;
 
-        int latasMisturadas = (int) Math.floor(quantidadeTinta / 18.0);
-        double restanteTinta = quantidadeTinta - (latasMisturadas * 18.0);
-        int galoesMisturados = (int) Math.ceil(restanteTinta / 3.6);
-        double precoMistura = (latasMisturadas * 80.0) + (galoesMisturados * 25.0);
+    System.out.println("Para pintar a área informada, será preciso " + latas18Litros + " latas de tinta.");
+    System.out.println("O valor gasto será de: R$ " + precoLata18Litros);
 
-    System.out.println("Quantidade de tinta necessária:");
-    System.out.println("Latas de 18 litros: " + latas18Litros);
-    System.out.println("Galões de 3,6 litros: " + galoes36Litros);
-
-    System.out.println("Comprando apenas latas de 18 litros custará: R$ " + precoLata18Litros);
-    System.out.println("Comprando apenas galões de 3,6 litros custará: R$ " + precoGalao36Litros);
-    System.out.println("Misturando latas e galões, o custo total será de: R$ " + precoMistura);
-    
     }
 }
--- Lista01/Ex017/src/App.java
+++ Lista01/Ex017/src/App.java
@@ -4,17 +4,30 @@ public class App {
     public static void main(String[] args) {
     Scanner teclado = new Scanner(System.in);
 
-    System.out.print("Informe o tamanho do arquivos (em MB)");
-        double tamanhoArquivo = teclado.nextDouble();
+    System.out.print("Digite o tamanho da área a ser pintada em metros quadrados");
+        double areaPintada = teclado.nextDouble();
 
-    System.out.print("Informe a velocidade de download da internet (em Mbps)");
-        double velocidadeDownload = teclado.nextDouble();
+        double coberturaTinta = 6.0; 
+        double quantidadeTinta = areaPintada / coberturaTinta;
+        quantidadeTinta = quantidadeTinta * 1.1;
+        int latas18Litros = (int) Math.ceil(quantidadeTinta / 18.0);
+        int galoes36Litros = (int) Math.ceil(quantidadeTinta / 3.6); 
 
-    double segundosDownload = tamanhoArquivo / velocidadeDownload;
+        double precoLata18Litros = latas18Litros * 80.0;
+        double precoGalao36Litros = galoes36Litros * 25.0;
 
-    double minutosDownload = segundosDownload / 60;
+        int latasMisturadas = (int) Math.floor(quantidadeTinta / 18.0);
+        double restanteTinta = quantidadeTinta - (latasMisturadas * 18.0);
+        int galoesMisturados = (int) Math.ceil(restanteTinta / 3.6);
+        double precoMistura = (latasMisturadas * 80.0) + (galoesMisturados * 25.0);
 
-    System.out.println(String.format("O download do arquivo demorará %.1f minutos", minutosDownload));
+    System.out.println("Quantidade de tinta necessária:");
+    System.out.println("Latas de 18 litros: " + latas18Litros);
+    System.out.println("Galões de 3,6 litros: " + galoes36Litros);
 
+    System.out.println("Comprando apenas latas de 18 litros custará: R$ " + precoLata18Litros);
+    System.out.println("Comprando apenas galões de 3,6 litros custará: R$ " + precoGalao36Litros);
+    System.out.println("Misturando latas e galões, o custo total será de: R$ " + precoMistura);
+    
     }
 }
